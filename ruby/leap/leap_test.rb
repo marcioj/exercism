bd28c460de2216f30ceb2df258c64c745ab5b771
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-05-16 21:25:37 -0300

    New stuff

---
+++ ruby/leap/leap_test.rb
@@ -0,0 +1,38 @@
+require 'date'
+require 'minitest/autorun'
+require_relative 'year'
+
+class Date
+  def leap?
+    throw "Try to implement this yourself instead of using Ruby's implementation."
+  end
+  
+  alias :gregorian_leap? :leap?
+  alias :julian_leap? :leap?
+end
+
+class YearTest < MiniTest::Unit::TestCase
+  def test_leap_year
+    assert Year.leap?(1996)
+  end
+
+  def test_non_leap_year
+    skip
+    refute Year.leap?(1997)
+  end
+  
+  def test_non_leap_even_year
+    skip
+    refute Year.leap?(1998)
+  end
+
+  def test_century
+    skip
+    refute Year.leap?(1900)
+  end
+
+  def test_fourth_century
+    skip
+    assert Year.leap?(2400)
+  end
+end
