01d64f10d65d16e3f7d3b7b5bc519ccb3dc29e55
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-05-16 21:53:18 -0300

    Elixir bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,10 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-
+      input == "" -> "Fine. Be that way."
+      String.upcase(input) == input -> "Woah, chill out!"
+      String.ends_with? input, "?" -> "Sure."
+      true -> "Whatever."
     end
   end
 end
