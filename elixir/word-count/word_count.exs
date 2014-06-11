193089dc19af1b279a0a157381952c3b526c3c89
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-06-11 20:54:53 -0300

    Make elixir word_count pass

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -1,4 +1,7 @@
 defmodule Words do
+  import String, only: [downcase: 1, split: 3, split: 2]
+  import Dict, only: [update: 4]
+  import Enum, only: [flat_map: 2]
   @doc """
   Count the number of words in the sentence.
 
@@ -6,6 +9,17 @@ defmodule Words do
   """
   @spec count(String.t) :: HashDict.t
   def count(sentence) do
-  
+    words = sentence |> downcase |> split(~r/[^\w-]/u, trim: true) |> flat_map(&(split(&1, "_")))
+    do_count(words, HashDict.new)
   end
+
+  defp do_count([current_word | words], hash) do
+    hash = update(hash, current_word, 1, &(&1+1))
+    do_count(words, hash)
+  end
+
+  defp do_count([], hash) do
+    hash
+  end
+
 end
